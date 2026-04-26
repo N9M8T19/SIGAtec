370d0bebb31c8694ddcc1481554129f2b25c0599
N9M8T19 <nicolas.montefinal@bue.edu.ar>
2026-04-26 19:39:24 -0300

    Fix: saca ubicaciones_bp que causaba ImportError en deploy

--- app.py
+++ app.py
@@ -92,7 +92,6 @@ def create_app():
     from routes.mantenimiento   import mantenimiento_bp
     from routes.asignaciones    import asignaciones_bp   # ⚠️ Nuevo 22/04/2026
     from routes.tvs             import tvs_bp            # ⚠️ Nuevo 24/04/2026
-    from routes.ubicaciones     import ubicaciones_bp    # ⚠️ Nuevo 24/04/2026
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(carros_bp)
@@ -116,7 +115,6 @@ def create_app():
     app.register_blueprint(mantenimiento_bp)
     app.register_blueprint(asignaciones_bp)              # ⚠️ Nuevo 22/04/2026
     app.register_blueprint(tvs_bp)                       # ⚠️ Nuevo 24/04/2026
-    app.register_blueprint(ubicaciones_bp)               # ⚠️ Nuevo 24/04/2026
 
     with app.app_context():
         import models_extra.horarios_notificaciones  # noqa
